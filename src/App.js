aa057de549447fb29467646d2526f7a501c54b40
Minwookim <trte135@naver.com>
2021-12-22 22:55:22 +0900

    2021.12.22 React.JS - Day 5(promise, onChange, onCreate, concat, onRemove )

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
+import CreateUser from './CreateUser';
 import UserList from './UserList';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username : '',
+    email : '',
+  });
+
+  const {username, email} = inputs;
+
+  const onChange = (e) => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name] : value,
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
         id : 1,
         username : 'velopert',
@@ -18,17 +34,35 @@ function App() {
         username : 'hero',
         email : 'herot@naver.com',
     },
-  ];
+  ]);
 
   const nextId = useRef(4); // useRef로 컴포넌트 안의 변수 만들기
 
   const onCreate = () => { // onCreate 함수가 실행되면(1)
-    console.log(nextId.current); // 4
+    const user = {
+      id : nextId.current,
+      username,
+      email,
+    };
+    // setUsers([...users, user]);
+    setUsers(users.concat(user));
+    setInputs({
+      username : '',
+      email : '',
+    });
+    
     nextId.current += 1; // 1씩 증가시켜라~!(2)
   };
 
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id)); // id가 일치하지 않으면?
+  };
+
   return (
-    <UserList users={users} />
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} onRemove={onRemove} />
+    </>
   );
 }
 
