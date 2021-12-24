6498948df904095508b778dee76cd3d721901eb6
Minwookim <trte135@naver.com>
2021-12-24 22:52:26 +0900

    2021.12.24 React.JS - Day 6(onToggle, map function, active)

--- src/App.js
+++ src/App.js
@@ -23,16 +23,19 @@ function App() {
         id : 1,
         username : 'velopert',
         email : 'public.velopert@gmail.com',
+        active : true,
     },
     {
         id : 2,
         username : 'test',
         email : 'test@gmail.com',
+        active : false,
     },
     {
         id : 3,
         username : 'hero',
         email : 'herot@naver.com',
+        active : false,
     },
   ]);
 
@@ -58,10 +61,16 @@ function App() {
     setUsers(users.filter(user => user.id !== id)); // id가 일치하지 않으면?
   };
 
+  const onToggle = id => {
+    setUsers(users.map(
+      user => user.id === id ? {...user, active : !user.active} : user
+    ))
+  };
+
   return (
     <>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-      <UserList users={users} onRemove={onRemove} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 // Array rendering
 
-function User ({user, onRemove}) { // 함수를 하나 더 생성해서 좀더 쉽게 작업해준다.
-    const {username, email, id} = user;
+function User ({user, onRemove, onToggle}) { // 함수를 하나 더 생성해서 좀더 쉽게 작업해준다.
+    const {username, email, id, active} = user;
     return(
         <div>
-            <b>{username}</b> <span>({email})</span>
+            <b style={{
+                color : active ? 'orange' : '#000',
+                cursor : 'pointer'
+            }} onClick={ () => onToggle(id)} >{username}</b> &nbsp; <span>({email})</span>
             <button onClick={ () => onRemove(id)}>삭제</button>
         </div>
     );
 };
 
-function UserList ({users, onRemove}) {
+function UserList ({users, onRemove, onToggle}) {
     return ( // map함수를 사용해서 배열을 불러와준다!!
         <div>
             {
@@ -20,7 +23,7 @@ function UserList ({users, onRemove}) {
                     // (user, index) => (<User key={index} user={user} />) 기본 index값을 주므로써
                     // 랜더링을 할 수 있다. 그러나 key안에 index값 넣는 걸 추천하지 않는다!
                     // key값이 있어야지만 효율적으로 랜더링 할 수 있다.
-                    user => (<User key={user.id} user={user} onRemove={onRemove} />)
+                    user => (<User key={user.id} user={user} onRemove={onRemove} onToggle={onToggle} />)
                 )
             }
         </div>
