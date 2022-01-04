199e9e7c1402d61fab91da8772339cdc175b7ce9
Minwookim <trte135@naver.com>
2022-01-04 23:53:34 +0900

    2022.1.4 React.JS - Day 14 (Prettier, ELNint)

---
+++ useful-tools/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
+/*
+  [Prettier]
+
+  React 작업할 때 좀더 편리하게 해주는 역할(setting)
+
+  [ESLint]
+
+  1. parameter 갯수를 제한해주는 작업
+  2. eqeq(문법적으로 오류가 있는지까지 테스트해 준다.)
+  3. parameter를 다른 곳에서 재사용이 금지! 단, 새로운 변수를 만들어서 주는 것이면 가능!
+  4. 분기문 작성시에도 최대한으로 간편하게 해준다.
+  5. variable를 설정해놓고 사용하지 않으면 경고까지 띄어준다.
+  6. semicolon 규칙성도 통일성을 강조한다.
+  7. quote(''/"") 규칙성도 통일성을 강조한다.
+  8. variable 설정할때도 규칙성를 강조해주는 역할도 함!
+
+*/
