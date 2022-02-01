d0649bb6b534bc4790e77590f83efe8d8f84db10
Minwookim <trte135@naver.com>
2022-02-01 23:31:08 +0900

    2022.2.1 React.JS - Day 17 (styled-components, styled-components button, styled-components-Polished, styled-components button size,  styled-components button outline,fullWidth)

--- styling-with-styled-components/src/App.js
+++ styling-with-styled-components/src/App.js
@@ -1,26 +1,85 @@
-import React from "react";
-import "./App.css";
-import styled, { css } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import Button from "./components/Button";
 
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: ${(props) => props.color};
-  border-radius: 50%;
-  ${(props) =>
-    props.huge &&
-    css`
-      width: 10rem;
-      height: 10rem;
-    `}
+// Components style
+// const Circle = styled.div`
+//   width: 20rem;
+//   height: 20rem;
+//   background: ${(props) => props.color};
+//   border-radius: 50%;
+//   ${(props) =>
+//     props.huge &&
+//     css`
+//       width: 10rem;
+//       height: 10rem;
+//     `}
+// `;
+
+// button size
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid #000;
+  padding: 1rem;
 `;
+
+// button group
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+// color
+const palette = {
+  blue: "#228be6",
+  gray: "#6F6F6F",
+  pink: "#f06595",
+};
+
+// size
+const size = {
+  large: "3",
+  medium: "2.25",
+  small: "1.75",
+};
 
 function App() {
   return (
-    <>
-      <Circle color="black" />
-      <Circle color="blue" huge />
-    </>
+    <ThemeProvider theme={{ palette, size }}>
+      <AppBlock>
+        <ButtonGroup>
+          <Button size="large">Button</Button>
+          <Button color="gray">Button</Button>
+          <Button color="pink" size="small">
+            Button
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large" outline>
+            Button
+          </Button>
+          <Button color="gray" outline>
+            Button
+          </Button>
+          <Button color="pink" size="small" outline>
+            Button
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large" fullWidth>
+            Button
+          </Button>
+          <Button color="gray" size="large" fullWidth>
+            Button
+          </Button>
+          <Button color="pink" size="large" fullWidth>
+            Button
+          </Button>
+        </ButtonGroup>
+      </AppBlock>
+    </ThemeProvider>
   );
 }
 
