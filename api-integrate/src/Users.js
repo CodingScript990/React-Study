07772bad8935dd365efb2a896ca3e8f3e931493e
Minwookim <trte135@naver.com>
2022-02-05 22:16:39 +0900

    2022.2.5 React.JS - Day 20 - add (API interlock[Basic study - axios], App.js, Users.js)

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -9,18 +9,52 @@ function Users() {
   // Error
   const [error, setError] = useState(null);
 
+  // axios
+  const fetchUsers = async () => {
+    try {
+      setUsers(null); // initial user value === null
+      setError(null); // initial error value === null
+      setLoading(true); // user start(1)
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users/"
+      );
+      // data value
+      setUsers(response.data);
+    } catch (e) {
+      // error
+      console.log("Error : ", e.response.status);
+      setError(e);
+    }
+    // loading === false
+    setLoading(false);
+  };
+
   // Rendering
   useEffect(() => {
-    // axios
-    const fetchUsers = async () => {
-      try {
-        setUsers(null);
-        setError(null);
-        setLoading(true); // user start(1)
-      } catch (e) {}
-    };
+    fetchUsers(); // callback function
   }, []);
-  return <div></div>;
+
+  // [false]
+  // loading code
+  if (loading) return <div>loading..</div>;
+  // error code
+  if (error) return <div>Sorry, An error has occurred.</div>;
+  // not users value === null
+  if (!users) return null;
+
+  // true
+  return (
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}>Bring</button>
+    </>
+  );
 }
 
 export default Users;
